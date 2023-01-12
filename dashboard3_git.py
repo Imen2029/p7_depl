5b828a7237c0358753785f70d720c497f42b671b
Imen2029 <98740428+Imen2029@users.noreply.github.com>
2023-01-12 10:15:11 +0100

    Add files via upload

--- dashboard3_git.py
+++ dashboard3_git.py
@@ -20,7 +20,7 @@ import streamlit.components.v1 as components
 
 #API_URL='http://127.0.0.1:5000/'
 #API_URL='http://0.0.0.0:$PORT'
-API_URL='http://192.168.1.19:8501'
+API_URL='http://192.168.1.19:8501/'
 #load the model
 infile1=open('LGBMClassifier_f2score_is_unbalance.pkl','rb')
 model=pickle.load(infile1)
