4b96026e1b245e31dea4a83c85df83cdf5259f44
Imen2029 <imenkadri198@gmail.com>
2023-01-12 14:35:25 +0100

    first deploy in streamlit cloud

--- dashboard3_git.py
+++ dashboard3_git.py
@@ -18,8 +18,8 @@ from matplotlib import pyplot as plt
 import shap
 import streamlit.components.v1 as components
 
-#API_URL='http://127.0.0.1:5000/'
-API_URL='http://0.0.0.0:$PORT/'
+API_URL='http://127.0.0.1:5000/'
+#API_URL='http://0.0.0.0:$PORT/'
 
 #load the model
 infile1=open('LGBMClassifier_f2score_is_unbalance.pkl','rb')
